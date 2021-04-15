d0c5b3f318fb0ed82228399bdc406c74b210a222
Andrés Hernández Mata <andreshernandezmta@gmail.com>
2021-04-14 19:52:52 -0500

    Actualizando diseño
    Version 2.0.0

--- src/udp_cliente.py
+++ src/udp_cliente.py
@@ -1,27 +1,41 @@
 """
 Uso: Cliente
 Creado: Andrés Hernández Mata
-Version: 1.0.0
+Version: 2.0.0
 Python: 3.9.1
 Fecha: 07 Abril 2020
 """
 
 import socket
 import sys
+import os
+from colorama import Fore
+from colorama import Style
+import pyfiglet as header
+from termcolor import colored
+
+os.system("cls")
 
 servidor = ("127.0.0.1", 2000)
 bufferSize = 1024
 
+banner = header.figlet_format("CLIENTE UDP").upper()
+print(colored(banner.rstrip("\n"), 'yellow', attrs=['bold']))
+
 UDPClientSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-while(True):
-    pregunta = input("Ingrese una pregunta: ")
-    if pregunta == "bye":
-        UDPClientSocket.close()
-        sys.exit()
-    else:
-        UDPClientSocket.sendto(str.encode(pregunta), servidor)
-        mensaje = UDPClientSocket.recvfrom(bufferSize)
-        respuesta = "Respuesta: " + mensaje[0].decode()
-        print(respuesta)
-
+try:
+    while True:
+        pregunta = input("Ingrese una pregunta: ")
+        if pregunta == "bye":
+            UDPClientSocket.sendto(str.encode(pregunta), servidor)
+            UDPClientSocket.close()
+            print("Saliendo...")
+            sys.exit()
+        else:
+            UDPClientSocket.sendto(str.encode(pregunta), servidor)
+            mensaje = UDPClientSocket.recvfrom(bufferSize)
+            respuesta = "Respuesta: " + mensaje[0].decode()
+            print(respuesta)
+except Exception:
+    print(Fore.RED + "Ocurrio una excepción en la conexion del servidor" + Style.RESET_ALL)
--- src/udp_servidor.py
+++ src/udp_servidor.py
@@ -1,31 +1,47 @@
 """
 Uso: Servidor
 Creado: Andrés Hernández Mata
-Version: 1.0.0
+Version: 2.0.0
 Python: 3.9.1
 Fecha: 07 Abril 2020
 """
 
 import socket
+import sys
+import os
+from colorama import Fore
+from colorama import Style
+import pyfiglet as header
+from termcolor import colored
+
+os.system("cls")
 
 localIP = "127.0.0.1"
 localPort = 2000
 bufferSize = 1024
 
-UDPServerSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+banner = header.figlet_format("SERVIDOR UDP").upper()
+print(colored(banner.rstrip("\n"), 'yellow', attrs=['bold']))
 
+UDPServerSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 UDPServerSocket.bind((localIP, localPort))
 print("El servidor UDP listo para recibir preguntas del cliente")
 
-
-while(True):
-    recibido = UDPServerSocket.recvfrom(bufferSize)    
-    mensaje = recibido[0]
-    ip = recibido[1]    
-    cliente = recibido[1][0]
-    print("Pregunta: " + mensaje.decode())
-    print("Cliente: " + cliente)
-    respuesta = input("Ingrese una respuesta: ")
-    UDPServerSocket.sendto(str.encode(respuesta), ip)
-
+try:
+    while True:
+        recibido = UDPServerSocket.recvfrom(bufferSize)    
+        mensaje = recibido[0]
+        mensaje = mensaje.decode()
+        if mensaje == "bye":
+            UDPServerSocket.close()
+            sys.exit()
+        else:
+            ip = recibido[1]
+            cliente = recibido[1][0]
+            print("Pregunta: " + mensaje)
+            print("Cliente: " + cliente)
+            respuesta = input("Ingrese una respuesta: ")
+            UDPServerSocket.sendto(str.encode(respuesta), ip)
+except Exception:
+    print(Fore.RED + "Ocurrio una excepción en la conexion del cliente" + Style.RESET_ALL)
 
