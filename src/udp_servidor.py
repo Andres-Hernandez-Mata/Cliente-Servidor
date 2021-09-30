9d89420f416f3a3d6a4b19be601b2e3ece69d511
Andrés Hernández Mata <andreshernandezmta@gmail.com>
2021-09-30 18:15:51 -0500

    Creador

--- src/udp_servidor.py
+++ src/udp_servidor.py
@@ -1,6 +1,6 @@
 """
 Uso: Servidor
-Creado: Andrés Hernández Mata
+Creador: Andrés Hernández Mata
 Version: 2.0.0
 Python: 3.9.1
 Fecha: 07 Abril 2020
@@ -29,7 +29,7 @@ print("El servidor UDP listo para recibir preguntas del cliente")
 
 try:
     while True:
-        recibido = UDPServerSocket.recvfrom(bufferSize)    
+        recibido = UDPServerSocket.recvfrom(bufferSize)
         mensaje = recibido[0]
         mensaje = mensaje.decode()
         if mensaje == "bye":
@@ -44,4 +44,3 @@ try:
             UDPServerSocket.sendto(str.encode(respuesta), ip)
 except Exception:
     print(Fore.RED + "Ocurrio una excepción en la conexion del cliente" + Style.RESET_ALL)
-
